4f18be979401364b0b0670bb901e685d6134f4c5
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-09-06 11:47:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openYoutube() {
+    window.open('https://youtube.com/@akshay.uproject?si=ARYcBUhtvPnvS-Li', '_blank');
+}
+
+function openInstagram() {
+    window.open('https://www.instagram.com/akshay.uproject?igsh=MTN6MzhrcGdraTdjMQ==', '_blank');
+}
