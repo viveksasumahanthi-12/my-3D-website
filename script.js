06fc11f04d9d9014920d31e32027f90be5159980
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-09-30 10:46:16 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-function openYoutube() {
-    window.open('https://youtube.com/@akshay.uproject?si=ARYcBUhtvPnvS-Li', '_blank');
-}
-
-function openInstagram() {
-    window.open('https://www.instagram.com/akshay.uproject?igsh=MTN6MzhrcGdraTdjMQ==', '_blank');
-}
