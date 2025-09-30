d9fa2fbe0fdf1dcf67b18b445099a0f5d7968319
viveksasumahanthi-12 <viveksasumahanthi@gmail.com>
2025-09-30 10:51:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Simple smooth scroll (optional enhancement)
+document.querySelectorAll('a.nav-link').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    target.scrollIntoView({ behavior: 'smooth' });
+  });
+});
